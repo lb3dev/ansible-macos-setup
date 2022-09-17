0a23473f418cb0291deffec40490fb7237512323
lb3dev <lben3dev@gmail.com>
2022-09-17 17:45:56 -0400

    Added logic in the initial script to determine the system Python path and add to PATH variable

--- initial_setup.sh
+++ initial_setup.sh
@@ -6,7 +6,13 @@ set -o pipefail
 
 # Set path for this script session
 
-export PATH="$HOME/Library/Python/3.8/bin:$PATH:/opt/homebrew/bin"
+MACOS_VERSION=$(sw_vers -productVersion)
+
+if [[ $MACOS_VERSION == 12.6 ]]; then
+  export PATH="$HOME/Library/Python/3.9/bin:/opt/homebrew/bin:$PATH"
+else
+  export PATH="$HOME/Library/Python/3.8/bin:/opt/homebrew/bin:$PATH"
+fi
 
 # Install ansible via pip
 
