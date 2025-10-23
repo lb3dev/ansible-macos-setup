6d3fee198fe163a1c279cec03af0ce65783659f9
lb3dev <lben3dev@gmail.com>
2025-10-22 20:41:37 -0400

    Updated python 3.13 installed path for latest Homebrew python installations

--- initial_setup.sh
+++ initial_setup.sh
@@ -41,11 +41,11 @@ brew analytics off
 set +v
 
 # Install latest Python 3
+set -x
 brew install python@3.13
 
 # Setup virtualenv, update pip and install ansible and dependencies
-set -x
-/usr/local/bin/python3 -m venv ~/.setup/venv-ansible
+/usr/local/bin/python3.13 -m venv ~/.setup/venv-ansible
 
 set +x
 source ~/.setup/venv-ansible/bin/activate
